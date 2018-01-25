1b8a81efa320cc2f499c841cf55054b4c0e31474
Max Meijer <meijermax99@gmail.com>
2018-01-25 14:41:37 +0100

    Modify build script so that it doesnt run when compilation fails

--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
 cd src
 ghc --make Main.hs
-Main ..\test-cases\01-test-task01.cs
-#.\ssm.bat --stdin
+IF %ERRORLEVEL% EQU 0 (
+    Main ..\test-cases\01-test-task01.cs
+)
+
+IF %ERRORLEVEL% EQU 0 (
+        cd ..\ssm-20150616
+        java -jar .\ssm.jar --file C:\Users\Max\source\repos\HaskellC#\test-cases\01-test-task01.ssm
+    )
